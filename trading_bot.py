2759d474f41efe53e683f6bf53a2ad8128bb0f8f
vijaychavan1842-ux <vijaychavan1842@gmail.com>
2026-07-16 21:16:56 +0530

    Update trading_bot.py

--- trading_bot.py
+++ trading_bot.py
@@ -14,9 +14,14 @@ CHAT_ID = os.getenv("538248415")
 def send_telegram(msg):
     try:
         url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-        requests.post(url, json={"chat_id": CHAT_ID, "text": msg})
-    except:
-        print("Telegram Error")
+        response = requests.post(
+            url,
+            json={"chat_id": CHAT_ID, "text": msg},
+            timeout=10
+        )
+        print("Telegram Response:", response.text)
+    except Exception as e:
+        print("Telegram Failed:", e)
 
 def send_csv():
     try:
